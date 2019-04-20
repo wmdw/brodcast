e6d1f2186083fa124c052d5397206e1f8d33999d
wmdw <49795721+wmdw@users.noreply.github.com>
2019-04-20 04:24:35 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`BrodCast Tags`,"http://twitch.tv/Death Shop")
+client.user.setGame(``,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
